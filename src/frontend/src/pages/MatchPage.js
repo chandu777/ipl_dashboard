e7b8fdcaea7f7d8c4d2af4135b33ce0633b9e1a1
Chandrakant <ca03430@gmail.com>
2021-05-09 16:01:44 +0530

    added react team page.

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -6,9 +6,25 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+  const [matches, setMatches] = useState([]);
+  const {teamName, year} = useParams();
+
+  useEffect(
+    () => {
+      const fetchMatches = async () => {
+        const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+        const data = await response.json();
+        setMatches(data);
+      };
+      fetchMatches();
+    }, [teamName]
+  );
+
   return (
     <div className="MatchPage">
-      <h1>MatchPage</h1>
+      {
+        matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
+      }
     </div>
   );
 }
