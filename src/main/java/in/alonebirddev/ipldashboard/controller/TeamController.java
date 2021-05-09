e1cf384d4622c04e762c871b9c1f28346efe4d02
Chandrakant <ca03430@gmail.com>
2021-05-09 13:35:42 +0530

    added matches by year endpoint.

--- src/main/java/in/alonebirddev/ipldashboard/controller/TeamController.java
+++ src/main/java/in/alonebirddev/ipldashboard/controller/TeamController.java
@@ -3,11 +3,18 @@
  */
 package in.alonebirddev.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import in.alonebirddev.ipldashboard.model.Match;
 import in.alonebirddev.ipldashboard.model.Team;
 import in.alonebirddev.ipldashboard.repository.MatchRepository;
 import in.alonebirddev.ipldashboard.repository.TeamRepository;
@@ -34,5 +41,15 @@ public class TeamController {
 		Team team = this.teamRepositroy.findByTeamName(teamName);
 		team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
 		return team;
+	}
+	
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesByTeamAndYear(@PathVariable String teamName, @RequestParam int year) {
+		
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year+1, 1, 1);
+		
+		List<Match> matches = matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+		return matches;
 	}
 }
